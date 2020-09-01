ff875c414ba3b0a3dfcd4342add97f56d60a1e1b
Valeriy <val_korolyov@mail.ru>
2020-09-01 21:36:59 +0600

    Добавил ДЗ

---
+++ ConsoleApp61/ConsoleApp61/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp61
+{
+    class Program
+    {
+        const double MIN_DIVIDER = 2;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа поиска минимального делителя");
+            bool check = false;
+            int number = 0;
+            do
+            {
+                Console.WriteLine("Введите натуральное число");
+                string userNumber = Console.ReadLine();
+                try
+                {
+                    number = Convert.ToInt32(userNumber);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                if (number > 0)
+                    check = true;
+            } while (!check);
+
+            int devider = 0;
+            for(double i = MIN_DIVIDER; i <= number; i++)
+            {
+                double temp = number / i;
+                if(temp == (int) temp)
+                {
+                    devider = (int)i;
+                    break;
+                }
+            }
+            if(number == devider)
+            {
+                Console.WriteLine($"Число {number} - простое");
+            }
+            else
+            {
+                Console.WriteLine($"Минимальный делитель числа {number}: {devider}");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
